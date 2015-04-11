b339fe97facd00cf7ef96dd2c6349b55bbbc0abe
Anbuselvan Periannan <anbuselvan.p@gmail.com>
2015-04-11 16:06:18 -0700

    updated permission to update the npm modules

--- scripts/linux/setup-env.sh
+++ scripts/linux/setup-env.sh
@@ -4,7 +4,8 @@ mkdir -p /${HOME}/meteor/<%= appName %>/
 mkdir -p /${HOME}/meteor/<%= appName %>/config
 mkdir -p /${HOME}/meteor/<%= appName %>/tmp
 
-sudo chown ${USER} /etc/init
-sudo chown ${USER} /etc/
-
 sudo npm install -g forever userdown wait-for-mongo node-gyp
+
+sudo chown ${USER}: /${HOME} -R
+sudo chown ${USER}: /etc/init
+sudo chown ${USER}: /etc/
