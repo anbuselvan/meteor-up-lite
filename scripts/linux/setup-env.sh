5634c039bf876b8a7baa52805418a6a259115edc
Anbuselvan Periannan <anbuselvan.p@gmail.com>
2015-04-11 15:48:28 -0700

    updated permission to write insde /etc/init folder

--- scripts/linux/setup-env.sh
+++ scripts/linux/setup-env.sh
@@ -4,4 +4,7 @@ mkdir -p /${HOME}/meteor/<%= appName %>/
 mkdir -p /${HOME}/meteor/<%= appName %>/config
 mkdir -p /${HOME}/meteor/<%= appName %>/tmp
 
+sudo chown ${USER} /etc/init
+sudo chown ${USER} /etc/
+
 sudo npm install -g forever userdown wait-for-mongo node-gyp
